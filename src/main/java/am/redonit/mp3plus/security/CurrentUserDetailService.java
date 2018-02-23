7994b846d6fa7d9c8db8e19ce4d2045ee0fb1228
mrhamtalyan <alllllo@inbox.ru>
2018-02-24 03:24:59 +0400

    add

--- src/main/java/am/redonit/mp3plus/security/CurrentUserDetailService.java
+++ src/main/java/am/redonit/mp3plus/security/CurrentUserDetailService.java
@@ -1,7 +1,7 @@
 package am.redonit.mp3plus.security;
 
-import com.worldmusic.worldmusic.model.User;
-import com.worldmusic.worldmusic.repository.UserRepository;
+import am.redonit.mp3plus.model.User;
+import am.redonit.mp3plus.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
